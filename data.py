173016f119147bbefb0ceb1a7be5d1f70dcf1396
natebomar <nate.bomar@gmail.com>
2022-02-17 08:45:04 -0800

    updating data.py

--- data.py
+++ data.py
@@ -1,1 +1,24 @@
-print("Hello")
+import requests
+import json
+
+# auth_url = 'https://api.covid19api.com/auth/subscriptions'
+summary_url = 'https://api.covid19api.com/summary' 
+
+# r_auth = requests.get(auth_url, auth = {"Email":"nate.bomar@gmail.com", "Subscription": "basic"})
+
+
+payload={}
+headers = {}
+
+response = requests.request("GET", summary_url, headers=headers, data=payload)
+data = response.json()
+Countries = data["Countries"]
+# print(Countries[8])
+United_States = {}
+for country in Countries:
+    for (key, value) in country.items():
+        if value == "US":
+            United_States = country
+print(United_States)
+
+
