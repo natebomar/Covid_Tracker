87da53d2ac474fe2a15741b749fd31054b5133ed
natebomar <nate.bomar@gmail.com>
2022-02-24 09:55:55 -0800

    Merge branch 'main' of https://github.com/natebomar/Covid_Tracker

--- data.py
+++ data.py
@@ -1,5 +1,15 @@
 import requests
+import firebase_admin
+from firebase_admin import db
 import json
+
+
+#FireBase setup
+cred_obj = firebase_admin.credentials.Certificate('covidtrack_key.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': 'https://covid-track-45cd7-default-rtdb.firebaseio.com/'
+})
+
 
 # auth_url = 'https://api.covid19api.com/auth/subscriptions'
 summary_url = 'https://api.covid19api.com/summary' 
@@ -19,6 +29,21 @@ for country in Countries:
     for (key, value) in country.items():
         if value == "US":
             United_States = country
-print(United_States)
+print(United_States.items())
 
+# def data_to_html_table(data):
+#     html = '<table><tbody>'
+#     for (key,value) in data.items():
+#         html += '<tr><td>' + str(value) + '</td></tr>'
+#     html += '</tbody></table>'
+#     return html
 
+# print(data_to_html_table(United_States))
+
+with open("US_Summary.json", "w") as file:
+    file.write(United_States)
+
+ref = db.reference("/")
+with open("US_Summary.json", "r") as f:
+    summary = json.load(f)
+ref.set(summary)
