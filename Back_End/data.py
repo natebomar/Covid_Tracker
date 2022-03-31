cf4396d745901bcec4e787e21ccf2647a5b158ca
natebomar <nate.bomar@gmail.com>
2022-03-31 09:50:29 -0700

    pushing data file so that it doesn't get lost

--- Back_End/data.py
+++ Back_End/data.py
@@ -2,53 +2,57 @@ import requests
 import firebase_admin
 from firebase_admin import db
 import json
-
+import urllib.request
 
 #FireBase setup
-cred_obj = firebase_admin.credentials.Certificate('covidtrack_key.json')
-default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL': 'https://covid-track-45cd7-default-rtdb.firebaseio.com/'
-})
+# cred_obj = firebase_admin.credentials.Certificate('covidtrack_key.json')
+# default_app = firebase_admin.initialize_app(cred_obj, {
+#     'databaseURL': 'https://covid-track-45cd7-default-rtdb.firebaseio.com/'
+# })
 
 
-# auth_url = 'https://api.covid19api.com/auth/subscriptions'
+# Covid 19 USA Setup
 summary_url = 'https://api.covid19api.com/summary' 
-
-# r_auth = requests.get(auth_url, auth = {"Email":"nate.bomar@gmail.com", "Subscription": "basic"})
-
-
 payload={}
 headers = {}
-
 response = requests.request("GET", summary_url, headers=headers, data=payload)
-data = response.json()
-Countries = data["Countries"]
-# print(Countries[8])
-United_States = {}
-for country in Countries:
-    for (key, value) in country.items():
-        if value == "US":
-            United_States = country
-# print(United_States.items())
-
-ref = db.reference("/")
-ref.delete()
-
-# print(data_to_html_table(United_States))
-with open("us_summary.json", "w") as file:
-    file.write(str(United_States).replace("'",'"'))
-
-with open("us_summary.json", "r") as f:
-    unordered = f.read().split(",")
 
 
-with open("us_summary.json", "w") as order:
-    for _ in unordered:
-        order.write(_)
-        if _ != unordered[len(unordered)-1]:
-            order.write(",\n")
+# Covid 19 Los Angeles Setup
+LA_url = 'https://data.chhs.ca.gov/api/3/action/datastore_search?resource_id=046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a&q=Los%20Angeles'  
+fileobj = urllib.request.urlopen(LA_url)
+response_dict = json.loads(fileobj.read())
+# print(response_dict.keys())
+## prints: "dict_keys(['help', 'success', 'result'])"
+# print(response_dict['result'])
+## prints list of all LA Data ****USEFUL FOR ALL TIME****
+# print(type(response_dict['result']))
+## returns "<class 'dict>"
+# print(response_dict['result'].keys())
+## prints: "dict_keys(['include_total', 'resource_id', 
+## 'fields', 'records_format', 'q', 'records', 'limit', '_links', 'total'])"
 
-ref = db.reference("/")
-with open("us_summary.json", "r") as file:
-    current_sum = json.load(file)
-ref.push(current_sum)
+
+
+# # Fetch US Summary
+# data = response.json()
+# Countries = data["Countries"]
+# United_States = {}
+# for country in Countries:
+#     for (key, value) in country.items():
+#         if value == "US":
+#             United_States = country
+
+# # Send US Summary to Firebase
+# ref = db.reference("/")
+# ref.delete()
+# with open("us_summary.json", "w") as file:
+#     json.dump(United_States, file, indent=4)
+
+# ref = db.reference("/")
+# with open("us_summary.json", "r") as file:
+#     current_sum = json.load(file)
+# ref.push(current_sum)
+
+
+# # 
