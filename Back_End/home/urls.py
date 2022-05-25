96a466748936d6063f55e0577e837ba4f9bac15b
AdityaB1 <62115027+AdityaB1@users.noreply.github.com>
2022-05-24 22:13:50 -0700

    d

--- Back_End/home/urls.py
+++ Back_End/home/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 # URL Conf
 urlpatterns = [
-    path('index/index.html', views.us_sum),
-    path('index/today.html', views.us_today),
-    path('index/week.html', views.us_week),
-    path('index/month.html', views.us_month),
-    path('index/alltime.html', views.us_alltime),
-    path('index/vaccination.html', views.us_vaccination)
+    path('index.html', views.us_sum),
+    path('today.html', views.us_today),
+    path('week.html', views.us_week),
+    path('month.html', views.us_month),
+    path('alltime.html', views.us_alltime),
+    path('vaccination.html', views.us_vaccination)
 ]
