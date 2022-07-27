ca84b696f54a2d2dc503bcf357638f54bf0b7780
natebomar <nate.bomar@gmail.com>
2022-07-27 12:34:26 -0700

    Add heroku settings to production(prod.py)

--- Covid_Tracker/settings/prod.py
+++ Covid_Tracker/settings/prod.py
@@ -9,3 +9,35 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "covidtrackerla.herokuapp.com"
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d%b%Y %H:%M:%S"
+        },
+        'simple':{
+            'format': "%(levelname)s %(message)s"
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': "DEBUG"
+        }
+    }
+}
+
+# Heroku Settings
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
